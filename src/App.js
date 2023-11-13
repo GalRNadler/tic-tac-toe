1d5c3fa5b1985617e44aa4a572d28ae9bbfd2311
yoaaaav <yoavbenbassat@gmail.com>
2023-11-13 16:48:35 +0200

    prettier + useEffect definition

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import './App.css';
-import RestartButton from './RestartButton';
-import { lines } from './utils';
+import "./App.css";
+import RestartButton from "./RestartButton";
+import { lines } from "./utils";
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
@@ -10,10 +10,16 @@ function App() {
   const [gameOver, setGameOver] = useState(false);
   const [winner, setWinner] = useState(null);
 
+  useEffect(() => {}, [board]);
+
   const calculateWinner = (squares) => {
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
         return squares[a];
       }
     }
@@ -24,7 +30,7 @@ function App() {
     if (gameOver || board[index]) return;
 
     const newBoard = [...board];
-    newBoard[index] = xIsNext ? 'X' : 'O';
+    newBoard[index] = xIsNext ? "X" : "O";
     setBoard(newBoard);
     setXIsNext(!xIsNext);
 
@@ -41,7 +47,9 @@ function App() {
     </button>
   );
 
-  const status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? 'X' : 'O'}`;
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xIsNext ? "X" : "O"}`;
 
   const handleRestart = () => {
     setBoard(Array(9).fill(null));
@@ -54,7 +62,7 @@ function App() {
     <div className="App">
       <img src="logo.png" alt="Tic-Tac-Toe Logo" className="logo" />
       <div className="status">{status}</div>
-      
+
       {Array(3)
         .fill(null)
         .map((_, i) => (
@@ -64,10 +72,12 @@ function App() {
               .map((_, j) => renderSquare(i * 3 + j))}
           </div>
         ))}
-        
-      {gameOver && <RestartButton onClick={handleRestart} className="restart-button" />}  
+
+      {gameOver && (
+        <RestartButton onClick={handleRestart} className="restart-button" />
+      )}
     </div>
   );
-            }
+}
 
-export default App ;
+export default App;
